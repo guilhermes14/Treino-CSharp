09620c2931321d2b87a1ddd61148ef47cc3efb47
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-06 09:02:50 -0300

    Add files via upload

---
+++ ProblemaFormatacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProblemaFormatacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var meuSalarioString = "2000,00";
+            double meuSalarioDouble = Convert.ToDouble(meuSalarioString);
+            Console.WriteLine(meuSalarioString);
+            Console.WriteLine($"Meu Salário é : {meuSalarioDouble:C3}");*/
+            //problema \/
+
+            Console.WriteLine("Digite seu salário: ");
+            var Salario = Console.ReadLine();
+            double SalarioDouble = Convert.ToDouble(Salario);
+            //pode ser convertido tbm usando Parse
+            Console.WriteLine($"Seu salário é {SalarioDouble:C3}");
+        }
+    }
+}
