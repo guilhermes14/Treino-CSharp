a394ebfe48e912ae3549a3abf54714bfb51150f3
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-06 09:01:52 -0300

    Add files via upload

---
+++ FormataNumero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace FormataNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuSalario = 2000.00;
+
+            Console.WriteLine(meuSalario.ToString("C"));
+            Console.WriteLine(meuSalario.ToString("C2"));
+            Console.WriteLine(meuSalario.ToString("C3"));
+
+            Console.WriteLine(meuSalario.ToString("C4", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(meuSalario.ToString("C4", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine(meuSalario.ToString("C4", CultureInfo.CreateSpecificCulture("jp-JP")));
+            //utilizando interpolação
+            Console.WriteLine($"{meuSalario:C2}");
+
+            var idade = 18;
+
+            Console.WriteLine($"Minha idade é {idade:N5}");
+            Console.WriteLine($"Minha idade é {idade:N}");
+
+            var valorTotal = 1500;
+            Console.WriteLine($"Valor total a pagar: {valorTotal:F2}");
+             
+        }
+    }
+}
