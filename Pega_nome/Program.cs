76c945ee6919a5c126422ec89e0d87b28a64df58
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-06 07:52:02 -0300

    Add files via upload

---
+++ Pega_nome/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pega_nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe seu primeiro nome: ");
+            var primeiroNome = Console.ReadLine();
+            Console.WriteLine("Informe seu segundo nome: ");
+            var segundoNome = Console.ReadLine();
+            Console.WriteLine($"Seu nome é {primeiroNome} {segundoNome}");
+        }
+    }
+}
