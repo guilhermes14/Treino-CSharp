1864e61c49c07aa082498b4a4fea0c66281605a0
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-11 08:45:02 -0300

    Update Program.cs

--- Tempo_De_Vida/Program.cs
+++ Tempo_De_Vida/Program.cs
@@ -14,7 +14,7 @@ namespace Tempo_De_Vida
             var DiasVividos = dataAtual - data;
 
             Console.WriteLine($"Você viveu {DiasVividos.Days} dias.");
-            Console.WriteLine($"Você nasceu em uma {data:ddd}");
+            Console.WriteLine($"Você nasceu em um(a) {data:dddd}");
             //Tipos de datas possíveis
             Console.WriteLine($"A data atual é {dataAtual:d}");
             Console.WriteLine($"A data atual é {dataAtual:D}");
