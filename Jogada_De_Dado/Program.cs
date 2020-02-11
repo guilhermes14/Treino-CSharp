59d3d94d6291fe26eb6496383aa1b35fb0b60a67
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-11 09:31:35 -0300

    Add files via upload

---
+++ Jogada_De_Dado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Jogada_De_Dado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tente a sorte e acerte o lado do Dado:");
+            var numeroEsperado = int.Parse(Console.ReadLine());
+
+            var Dado = new Random();
+            var jogada = Dado.Next(1, 7);
+
+            if(numeroEsperado == jogada)
+            {
+                Console.WriteLine($"O dado deu {jogada}");
+                Console.WriteLine("Boa! Você acertou!");
+            }
+            else
+            {
+                Console.WriteLine($"O dado deu {jogada}");
+                Console.WriteLine("Você errou, tente novamente!");
+            }
+        }
+    }
+}
