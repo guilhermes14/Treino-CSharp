24878413fa6762a807c8aa54ff4e41d9265486bd
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-11 08:31:34 -0300

    Add files via upload

---
+++ Tempo_De_Vida/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tempo_De_Vida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe sua data de nascimento (dd/mm/yyyy):");
+            var DataNascimento = Console.ReadLine();
+
+            var data = DateTime.Parse(DataNascimento);
+            var dataAtual = DateTime.Now;
+            var DiasVividos = dataAtual - data;
+
+            Console.WriteLine($"Você viveu {DiasVividos.Days} dias.");
+        }
+    }
+}
