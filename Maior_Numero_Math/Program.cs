aa5c17547ce778a8163dab8bfa8b5c1606d1442e
Guilherme Saraiva <49844284+guilhermes14@users.noreply.github.com>
2020-02-11 09:30:03 -0300

    Add files via upload

---
+++ Maior_Numero_Math/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Maior_Numero_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe dois números: ");
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+
+            var MaiorNum = Math.Max(num1, num2);
+
+            Console.WriteLine($"O maior número é {MaiorNum}");
+        }
+    }
+}
